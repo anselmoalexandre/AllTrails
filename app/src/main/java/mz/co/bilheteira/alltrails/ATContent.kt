3a44258f0df3d17578a1965798630a84b6bc2194
Anselmo Alexandre <anselmo@snappmobile.io>
2024-10-17 11:58:21 +0200

    AllTrails content screen

--- app/src/main/java/mz/co/bilheteira/alltrails/ATContent.kt
+++ app/src/main/java/mz/co/bilheteira/alltrails/ATContent.kt
@@ -3,10 +3,8 @@ package mz.co.bilheteira.alltrails
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
